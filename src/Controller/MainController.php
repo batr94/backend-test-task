0a8e5cee727d7bd15bc4c3708f081633b2c011d2
batr94 <www.bat.94@gmail.com>
2024-07-31 23:06:29 +0500

    add action to calculate product price

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\CalculatePriceDTO;
+use App\Service\CalculateProductPriceService;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 
 final class MainController extends AbstractController
 {
-    #[Route('/calculate-price', name: 'app_calculate_price')]
-    public function calculatePrice()
-    {
+    #[Route('/calculate-price', name: 'app_calculate_price', methods: ['POST'])]
+    public function calculatePrice(
+        #[MapRequestPayload]
+        CalculatePriceDTO $calculatePriceRequestDTO,
+        CalculateProductPriceService $calculateProductPriceService
+    ): Response {
+        return new Response('' . $calculateProductPriceService->calculate($calculatePriceRequestDTO));
     }
 }
